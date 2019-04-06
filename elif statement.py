c3a622374351373921e7fd9c2937593d70b155d7
archirick <48961843+archirick@users.noreply.github.com>
2019-04-06 19:29:37 +0530

    Add files via upload

---
+++ elif statement.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a number : "))
+if n==10:
+    print("number equal to 10")
+elif n==50:
+    print("number equal to 50")
+elif n==100:
+    print("number equal to 100")
+else:
+    print("number is not equal to 10,50 and 100")
